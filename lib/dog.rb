51ab8e462726cd4f2918d4a9410ff32a980e7a68
Michael Rabon <nobar86@gmail.com>
2020-08-05 21:21:45 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -16,3 +16,20 @@ class Dog
   end 
 end
 
+class Person
+  def name=(persons_name)
+    @name = persons_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def job=(job)
+    @job = job
+  end
+  
+  def job
+    @job
+  end
+end
